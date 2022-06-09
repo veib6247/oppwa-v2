914c03ff3acb87a4cb94a5d123824c207cd04f63
Bryan Olandres <ozoneblacklight@outlook.com>
2022-06-09 19:56:18 +0800

    added notif for coming soon pages

--- src/views/Reporting.vue
+++ src/views/Reporting.vue
@@ -1,5 +1,20 @@
+<script>
+import TextNotif from "../components/TextNotif.vue";
+
+export default {
+  components: {
+    TextNotif,
+  },
+};
+</script>
+
 <template>
   <div class="box">
-    <h1 class="title">Reporting API!</h1>
+    <text-notif class="is-warning">
+      <h1 class="title is-size-6">
+        Reporting API page is currently under construction.
+      </h1>
+      <p class="subtitle is-size-7">Coming soon.</p>
+    </text-notif>
   </div>
 </template>
--- src/views/ServertoServer.vue
+++ src/views/ServertoServer.vue
@@ -1,5 +1,20 @@
+<script>
+import TextNotif from "../components/TextNotif.vue";
+
+export default {
+  components: {
+    TextNotif,
+  },
+};
+</script>
+
 <template>
   <div class="box">
-    <h1 class="title">Server-to-Server!</h1>
+    <text-notif class="is-warning">
+      <h1 class="title is-size-6">
+        Server-to-server page is currently under construction.
+      </h1>
+      <p class="subtitle is-size-7">Coming soon.</p>
+    </text-notif>
   </div>
 </template>
