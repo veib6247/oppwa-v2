16798c454a314c66c39c908b5d0bf611e617ef0d
Bryan Olandres <ozoneblacklight@outlook.com>
2022-06-15 16:01:36 +0800

    added default style for notification color

--- src/components/TextNotif.vue
+++ src/components/TextNotif.vue
@@ -3,13 +3,14 @@ export default {
   props: {
     colorType: {
       type: String,
+      default: "is-light",
     },
   },
 };
 </script>
 
 <template>
-  <div class="notification is-light raleway is-size-7" :class="colorType">
+  <div class="notification raleway is-size-7" :class="colorType">
     <slot></slot>
   </div>
 </template>
