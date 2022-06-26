024a4f09edeab5394a019b21b80e401acfd5ee7c
Bryan Olandres <ozoneblacklight@outlook.com>
2022-06-26 18:05:01 +0800

    added dash (-) to separate switch name and description

--- src/components/FormSwitch.vue
+++ src/components/FormSwitch.vue
@@ -37,7 +37,7 @@ export default {
       />
       <label :for="id"
         ><strong>{{ functionName }}</strong
-        >&nbsp;{{ label }}</label
+        >&nbsp;-&nbsp;{{ label }}</label
       >
     </div>
   </div>
